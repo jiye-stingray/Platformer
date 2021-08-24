3b6beefb5f3511b4911d5e0fcd12e7626f221863
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2021-08-24 11:15:46 +0900

    유튜브

---
+++ platformer/platformer/Assets/Scripts/GameManager.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    
+    public int totalPoint;
+    public int stagePoint;
+    public int stageIndex;
+    public int health;
+    public PlayerMove player;
+    public GameObject[] Stages;
+    public PlayerMove playerMove;
+
+    [Header("UI")]
+    public Image[] UIHealth;
+    public Text UIPoint;
+    public Text UIStage;
+    public GameObject UIRestartBtn;
+
+    void Update()
+    {
+        UIPoint.text = (totalPoint + stagePoint).ToString();
+    }
+    public void  NextStage()
+    {
+        //ChangeStage
+        if (stageIndex < Stages.Length-1)
+        {
+            Stages[stageIndex].SetActive(false);
+            stageIndex++;
+            Stages[stageIndex].SetActive(true);
+            PlayerReposition();
+
+            UIStage.text = "STAGE " + (stageIndex + 1);
+        }
+        else
+        {
+            //Game Clear
+            Time.timeScale = 0;
+
+            //Result UI
+            Debug.Log("게임 클리어!");
+            //ReStart UI Button
+            UIRestartBtn.SetActive(true);
+            Text btnText = UIRestartBtn.GetComponentInChildren<Text>();
+            btnText.text = "Clear!";
+            UIRestartBtn.SetActive(true);
+        }
+        
+
+        //Caculate Point
+        totalPoint += stagePoint;
+        stagePoint = 0;
+    }
+    public void HealthDown()
+    {
+        playerMove.PlaySound("DAMAGED");
+        if (health > 1)
+        {
+            health--;
+            UIHealth[health].color = new Color(1, 0, 0, 0.4f);
+        }
+        else
+        {
+            //All Health UI Off
+            UIHealth[0].color = new Color(1, 0, 0, 0.4f);
+
+
+            //Player Die Effect
+            player.OnDie();
+
+            //Result UI
+            Debug.Log("죽었습니다");
+
+            //Retry Button UI
+            UIRestartBtn.SetActive(true);
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            
+            if (health > 1)
+                //Player Reposition
+                PlayerReposition();
+            
+            //Health Down
+            HealthDown();
+
+
+        }
+    }
+
+    void PlayerReposition()
+    {
+        player.transform.position = new Vector3(0, 0, -1);
+        player.VelocityZero();
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+}
